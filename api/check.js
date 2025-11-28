eb27b70db036dd0a004556b983c1d8551c2b5e13
dvdtxfst68-star <dvdtxfst68@privaterelay.appleid.com>
2025-11-28 23:54:06 +0100

    Add blacklist support to API

--- api/check.js
+++ api/check.js
@@ -1,6 +1,11 @@
-// CD Hub Whitelist API
+// CD Hub Whitelist & Blacklist API
 const WHITELISTED_USERS = [
     "16773546",
+];
+
+const BLACKLISTED_USERS = [
+    // Add blacklisted user IDs here
+    // Example: "123456789",
 ];
 
 export default async function handler(req, res) {
@@ -13,10 +18,23 @@ export default async function handler(req, res) {
         return res.status(400).json({ allowed: false, message: "No userid provided" });
     }
     
+    // Check blacklist FIRST - blacklist takes priority
+    const isBlacklisted = BLACKLISTED_USERS.includes(userid.toString());
+    if (isBlacklisted) {
+        return res.status(200).json({ 
+            allowed: false,
+            blacklisted: true,
+            userId: userid,
+            message: "User is blacklisted"
+        });
+    }
+    
+    // Then check whitelist
     const isWhitelisted = WHITELISTED_USERS.includes(userid.toString());
     
     return res.status(200).json({ 
         allowed: isWhitelisted,
+        blacklisted: false,
         userId: userid
     });
 }
